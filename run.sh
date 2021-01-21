47e88a36fd04c341536108ebd13d4d50436371c3
Lushleaf <lushleaf@163.com>
2021-01-21 01:07:42 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ python simu1_generate_data.py --save_dir dataset/simu1 --num_eval 100 --num_tune
 
 # train and eval
 # -- to run all models/methods in numbers of datasets
-python main_batch.py --data_dir data_dir --save_dir save_dir --num_dataset num_dataset
+python main_batch.py --data_dir dataset/simu1 --save_dir save_dir --num_dataset num_dataset
 
 # -- to run a singe run of models/methods with one dataset
 # -- set --plt_adrf True for plot adrf curve
-python main.py --data_dir data_dir --save_dir save_dir --plt_adrf True
+python main.py --data_dir dataset/simu1/eval/0 --save_dir save_dir --plt_adrf True
